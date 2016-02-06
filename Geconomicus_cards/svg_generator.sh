4c7275b50c45c96b0b160cc462fe5819eb6565aa
galuel <galuel@glibre.org>
2016-02-06 14:01:08 +0100

    Diamond replaces Trophy in 4th level cards back @galuel

--- Geconomicus_cards/svg_generator.sh
+++ Geconomicus_cards/svg_generator.sh
@@ -14,9 +14,9 @@ sed s/levelx/gold/g < carte_back.svg > generated_svg/carte_gold_back.svg
 sed -i -e s/textex/G/g generated_svg/carte_gold_back.svg
 sed -i -e s/fondx/#AA6DA2/g generated_svg/carte_gold_back.svg
 
-sed s/levelx/trophy/g < carte_back.svg > generated_svg/carte_trophy_back.svg
-sed -i -e s/textex/T/g generated_svg/carte_trophy_back.svg
-sed -i -e s/fondx/#290E26/g generated_svg/carte_trophy_back.svg
+sed s/levelx/diamond/g < carte_back.svg > generated_svg/carte_diamond_back.svg
+sed -i -e s/textex/D/g generated_svg/carte_diamond_back.svg
+sed -i -e s/fondx/#EB6800/g generated_svg/carte_diamond_back.svg
 
 # Génération des premières cartes
 
@@ -32,9 +32,9 @@ sed s/levelx/gold/g < carte_front.svg > generated_svg/carte_gold1.svg
 sed -i -e s/textex/G/g generated_svg/carte_gold1.svg
 sed -i -e s/valuex/v1/g generated_svg/carte_gold1.svg
 
-sed s/levelx/trophy/g < carte_front.svg > generated_svg/carte_trophy1.svg
-sed -i -e s/textex/T/g generated_svg/carte_trophy1.svg
-sed -i -e s/valuex/v1/g generated_svg/carte_trophy1.svg
+sed s/levelx/diamond/g < carte_front.svg > generated_svg/carte_diamond1.svg
+sed -i -e s/textex/D/g generated_svg/carte_diamond1.svg
+sed -i -e s/valuex/v1/g generated_svg/carte_diamond1.svg
 
 # Génération des 12 cartes suivantes (ou plus : rajouter des valeurs.svg dans generated_svg/values, et augmentez 'seq 2 max')
 
@@ -43,5 +43,5 @@ do
    sed "s/v1/v$i/g" generated_svg/carte_bronze1.svg > generated_svg/carte_bronze$i.svg
    sed "s/v1/v$i/g" generated_svg/carte_silver1.svg > generated_svg/carte_silver$i.svg
    sed "s/v1/v$i/g" generated_svg/carte_gold1.svg > generated_svg/carte_gold$i.svg
-   sed "s/v1/v$i/g" generated_svg/carte_trophy1.svg > generated_svg/carte_trophy$i.svg
+   sed "s/v1/v$i/g" generated_svg/carte_diamond1.svg > generated_svg/carte_diamond$i.svg
 done
