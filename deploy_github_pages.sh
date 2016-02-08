af0fc60a0fb1331cabadec3ff0768e2583dfd2a8
Vincent Texier <vit@free.fr>
2016-02-08 18:44:40 +0100

    Add automatic deploy to github pages

---
+++ deploy_github_pages.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+current_dir=$(pwd);
+set -e && \
+cd ./rst/build/html/ && \
+touch .nojekyll && \
+remote_repo="https://${GH_TOKEN}@${GH_REF}" && \
+remote_branch="gh-pages" && \
+git init && \
+git config user.name "Travis CI" && \
+git config user.email "nobody@nobody.org" && \
+git add . && \
+git commit -m'build' && \
+git push --force --quiet $remote_repo master:$remote_branch > /dev/null 2>&1 && \
+rm -fr .git && \
+cd ${current_dir}
