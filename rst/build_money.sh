50729695472eb7fba79d4368908ed716e10cbdbe
Vincent Texier <vit@free.fr>
2016-04-06 20:09:31 +0200

    Add banknote sheets for cheap printing

--- rst/build_money.sh
+++ rst/build_money.sh
@@ -13,4 +13,27 @@ do
     # resize at 75 dpi pixels size
     convert "${file%.svg}.png" -resize 251x162 -background black \( +clone -shadow 60x${SHADOW_OFFSET}+${SHADOW_OFFSET}+${SHADOW_OFFSET} \) +swap \
           -compose Over -composite +repage  "${file%.svg}_preview.png"
+
+    # banknote sheet
+    montage "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+        "${file%.svg}.png" \
+         -tile 4x5 -geometry +1+1 "${file%.svg}_sheet.png"
 done
