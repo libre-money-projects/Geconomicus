d7a838c9cbac10f3368441d0634684ee083cfa70
galuel <galuel@glibre.org>
2016-06-23 11:30:46 +0200

    Correction logo path Signed-off-by: galuel <galuel@glibre.org>

--- rst/build_logo.sh
+++ rst/build_logo.sh
@@ -8,6 +8,7 @@ for file in build/html/_static/logo/*.svg
 do
     # inkscape convert svg to png
     inkscape -d ${DPI} -z -f "${file}" -w 400 -h 400 -e "${file%.svg}.png"
+    mv ${file%.svg}.png build/html/_static
     inkscape -d ${DPI} -z -f "${file}" -w 32 -h 32 -e "${file%.svg}_favicon.png"
     mv ${file%.svg}_favicon.png build/html/_static
 done
