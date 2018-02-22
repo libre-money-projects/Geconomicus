c402b0f1f733cdb1c1811a01ef538e7c90fce6e0
[1000i100] Millicent Billette <git@1000i100.fr>
2018-02-22 22:20:20 +0100

    2.4 -> 2.4.1

--- rst/source/conf.py
+++ rst/source/conf.py
@@ -54,7 +54,7 @@ copyright = '2016, vit, Ğaluel'
 # built documents.
 #
 # The short X.Y version.
-version = '2.4'
+version = '2.4.1'
 # The full version, including alpha/beta/rc tags.
 release = '2.4.1'
 
