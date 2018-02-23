3e5e916ffda49701ac73730746b059907103ddc0
[1000i100] Millicent Billette <git@1000i100.fr>
2018-02-23 05:29:56 +0100

    cross reference re-inverting build order

--- build.sh
+++ build.sh
@@ -3,22 +3,21 @@
 current_dir=$(pwd)
 cd rst;
 
-echo generate money in png from svg in html
-./build_money.sh
-
-echo build html document
+echo "build html document"
 make html;
 
-echo create folder for cards sets
-mkdir -p ./build/html/_static/cards
+echo "generate logo and favicon"
+./build_logo.sh
 
-# echo generate "revolution" cards set from inkscape layers
-# ./build_cards.py ./source/_static/cards/revolution ./build/html/_static/cards
-
-echo generate help sheets from cards and money
+echo "generate help sheets from cards and money"
 ./build_help_sheets.sh
 
-echo generate logo and favicon
-./build_logo.sh
+echo "generate money in png from svg in html"
+./build_money.sh
+
+echo "create folder for cards sets"
+mkdir -p ./build/html/_static/cards
+# echo "generate \"revolution\" cards set from inkscape layers"
+# ./build_cards.py ./source/_static/cards/revolution ./build/html/_static/cards
 
 cd ${current_dir};
