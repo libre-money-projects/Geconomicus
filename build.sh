30555e610f30c9034aace3775cbcbb3f6db8e760
[1000i100] Millicent Billette <git@1000i100.fr>
2018-02-23 05:25:42 +0100

    less error by changing build order (deps before what check them)

--- build.sh
+++ build.sh
@@ -3,22 +3,22 @@
 current_dir=$(pwd)
 cd rst;
 
-# build html document
-make html;
-
-# generate money in png from svg in html
+echo generate money in png from svg in html
 ./build_money.sh
 
-# create folder for cards sets
+echo build html document
+make html;
+
+echo create folder for cards sets
 mkdir -p ./build/html/_static/cards
 
-# generate "revolution" cards set from inkscape layers
+# echo generate "revolution" cards set from inkscape layers
 # ./build_cards.py ./source/_static/cards/revolution ./build/html/_static/cards
 
-# generate help sheets from cards and money
+echo generate help sheets from cards and money
 ./build_help_sheets.sh
 
-# generate logo and favicon
+echo generate logo and favicon
 ./build_logo.sh
 
 cd ${current_dir};
